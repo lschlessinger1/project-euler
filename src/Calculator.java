dca59cf08a82b17bdac779efbceb3aa61b83ce97
Louis Schlessinger <louschlessinger96@gmail.com>
2014-07-13 13:53:10 -0400

    added README, and solutions to the first two problems

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+/*
+ * Problems taken from https://projecteuler.net/
+ * 
+ * */
+
+/**
+ * @author lschlessinger1
+ * 
+ */
+public class Calculator {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.showAnswer(Problem1.getSolution());
+		calculator.showAnswer(Problem2.getSolution());
+	}
+
+	private void showAnswer(int answer) {
+		System.out.println(Integer.toString(answer));
+	}
+
+}
